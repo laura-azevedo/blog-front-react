1720fd7f7601e92fd6f1aa0452d547073dd54611
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-02 14:23:43 -0300

    feat: create Home component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            <div style={{
+                width: "100vw",
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <div>
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <h2>Sejam bem vindos!</h2>
+                        <p>Expressem aqui seus pensamentos e opiniões :)</p>
+                    </div>
+
+                    <div style={{
+                         width: "80vw",
+                         display: "flex",
+                         flexDirection: "column",
+                         alignItems: "center"
+                    }}>
+                        <img 
+                            src="https://i.imgur.com/VpwApCU.png" 
+                            alt="Imagem da Página Home" 
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home
