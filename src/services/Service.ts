f9bc072921bd2f53cd25b895c5aff5083c945780
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-11 16:13:48 -0300

    feat(Service): add consult service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,6 +4,11 @@ const api = axios.create({
     baseURL: 'https://blogpessoal-jy5z.onrender.com/'
 })
 
+export const consult = async (url: string, setData: Function) => {
+    const response = await api.get(url)
+    setData(response.data)
+}
+
 export const registerUser = async (url: string, dados: Object, setData: Function) => {
     const response = await api.post(url, dados)
     setData(response.data)
