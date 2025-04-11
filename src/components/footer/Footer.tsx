f0a743062ef2356511a76ed52a956cf16992fa33
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-11 11:49:29 -0300

    feat: implement routing

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
 
@@ -12,10 +12,13 @@ function Footer() {
                             Blog Pessoal | Copyright: {data}
                         </p>
                     <p className='text-lg'>Acesse nossas redes sociais</p>
+
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+
+                        <a href="https://www.linkedin.com/in/azevedolaura/" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+
                     </div>
                 </div>
             </div>
