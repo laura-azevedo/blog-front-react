58949bf0bc99c53ab15e442eeb4398b34e245e53
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-11 11:22:07 -0300

    feat: use tailwind

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,33 +1,17 @@
-import React from 'react'
-
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
+            <div className="w-screen flex justify-center">
                 <div>
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
+                    <div className="max-w-7xl flex flex-col items-center">
                         <h2>Sejam bem vindos!</h2>
                         <p>Expressem aqui seus pensamentos e opiniões :)</p>
                     </div>
 
-                    <div style={{
-                         width: "80vw",
-                         display: "flex",
-                         flexDirection: "column",
-                         alignItems: "center"
-                    }}>
-                        <img 
-                            src="https://i.imgur.com/VpwApCU.png" 
-                            alt="Imagem da Página Home" 
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
                             width="400px"
                         />
                     </div>
