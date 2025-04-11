e2ee73dc1584b556a101d0c33efe6e60915e91a3
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-11 18:03:14 -0300

    feat(App): add AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import { UserProvider } from './context/UserContext';
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import { AuthProvider } from './context/AuthContext'
+import Cadastro from './pages/cadastro/Cadastro'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
-
   return (
-    <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </UserProvider>
-  );
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
