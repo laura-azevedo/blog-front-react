7b5255d775d016ff47d7ac00a6fdb42d15371bcf
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-04-13 17:21:04 -0300

    feat: implement FormTema component and its route on App and Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import ListaTemas from './components/temas/listatemas/ListaTemas'
+import FormTema from './components/temas/formtema/FormTema'
 
 function App() {
   return (
@@ -21,6 +22,8 @@ function App() {
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
               <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
             </Routes>
           </div>
           <Footer />
